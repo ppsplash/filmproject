91823591edb14cf06ce3671b4896644b0376856e
Johannes Dach <johannesdach@gmail.com>
2025-12-15 22:47:50 +0100

    init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { loadPopularMovies } from "./features/movies.js";
 import { setupMovieEvents } from "./events/movieEvents.js";
+import { setupGlobalEvents } from "./events/globalEvents.js";
 
 loadPopularMovies(1); // 1 for page 1
 setupMovieEvents();
+setupGlobalEvents();
